d13fa8fb68ae3fe781fab9ed6c590c04b2be2947
Rayhan <mdrayhan8045@gmail.com>
2026-01-25 12:51:42 +0600

    top menu

--- app/src/main/java/com/example/trendtrove/dashboard/DashActivity.kt
+++ app/src/main/java/com/example/trendtrove/dashboard/DashActivity.kt
@@ -1,6 +1,12 @@
 package com.example.trendtrove.dashboard
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,8 +18,16 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.trendtrove.R
 import com.example.trendtrove.databinding.ActivityDashBinding
+import com.example.trendtrove.starter.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DashActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var firebaseAuth: FirebaseAuth
     lateinit var binding: ActivityDashBinding
     lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,20 +37,70 @@ class DashActivity : AppCompatActivity() {
 
         navController = findNavController(R.id.fragmentview)
 
-        val appbarconfig = AppBarConfiguration(setOf(
+        val appbarconfig = AppBarConfiguration(
+            setOf(
 
-             R.id.productuser,
+                R.id.productuser,
 
-             R.id.uploaduser,
+                R.id.uploaduser,
 
-             R.id.profileuser
-        ))
+                R.id.profileuser
+            )
+        )
 
-         binding.btmnavigation.setupWithNavController(navController)
+        binding.btmnavigation.setupWithNavController(navController)
 
-         setupActionBarWithNavController(navController,appbarconfig)
+        setupActionBarWithNavController(navController, appbarconfig)
 
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.topmenu, menu)
+
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.report -> {
+
+                Toast.makeText(this, "repot working", Toast.LENGTH_LONG).show()
+
+            }
+
+            R.id.setting -> {
+
+                Toast.makeText(this, "setting working", Toast.LENGTH_LONG).show()
+
+            }
+
+            R.id.logout -> {
+
+                firebaseAuth.signOut()
+
+               startActivity(Intent(this, MainActivity::class.java))
+
+               finish()
+
+            }
+
+
+        }
+
+
+        return super.onOptionsItemSelected(item)
 
 
     }
+
 }
